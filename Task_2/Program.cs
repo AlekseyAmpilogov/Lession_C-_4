87c87dac8f12064e5d89aa4c72a87643ec07d2ae
Aleksey <pilot-1981@yandex.ru>
2022-09-07 17:53:48 +0500

    add Task_2.1

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++) // i = строка int [i, j]
+    {
+        for (int j=0; j<matr.GetLength(1); j++) // j = столбец int [i, j]
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int [, ] matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++) // i = строка int [i, j]
+    {
+        for (int j=0; j<matr.GetLength(1); j++) // j = столбец int [i, j]
+        {
+            matr[i, j] = new Random().Next(1, 25); // заполнение массива [i, j] числами от 1 до 25
+        }
+    }
+}
+
+int [, ] matrix = new int [3, 4];
+PrintArray (matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray (matrix);
